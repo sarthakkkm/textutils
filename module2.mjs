b55e6d2829c3d96fa4df3aebb47132990ba298c0
Sarthak <sarthakmaheshwari95@gmail.com>
2022-08-26 15:33:13 +0530

    Ading all files

---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a="harry"
+const b="Rohan"
+const c="akssh"
+const d="aksndjsnh"
+
+export default d;
+export {a};
+export {c};
+export {b};
---
+++ src/components/TextForm.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        console.log("Uppercase was clicked" + text);
+        let newText=text.toUpperCase();
+        setText(newText)
+    }
+    const handleLowClick = ()=>{
+        console.log("Uppercase was clicked" + text);
+        let newText=text.toLowerCase();
+        setText(newText)
+    }
+    const handleOnChange = (event)=>{
+        console.log("On Change");
+        setText(event.target.value);
+    }
+    const [text, setText] =useState('');
+
+    // text ="new text"; Wrong way to change the state
+    // setText("new text"); Correct way to change the state
+
+    return (
+        <>
+    <div className="container">
+        <h1>{props.heading} </h1>
+        <div className="mb-3">
+        <textarea className="form-control" value= {text}  onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+    </div>
+    <div className="container my-4">
+    <h3>Your Text Summary</h3>
+    <p>{text.split(" ").length} words and {text.length} characters</p>
+    <p>{0.008 *text.split(" ").length } Minutes read</p>
+    <h3>Preview</h3>
+    <p>{text}</p>
+    </div>
+    </>
+
+  )
+}
